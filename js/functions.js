04a5b4c4f1842c57eb14df3101dd83040768fb84
Vysotskiy96 <tvgradskyj@gmail.com>
2023-10-15 21:17:51 +0300

    Add molules and update function.js

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,20 @@ function getNum(string) {
 }
 
 getNum(-1);
+
+const convertsHoursToMinutes = (string) => {
+  string = string.split(':');
+
+  return +string[0] * 60 + +string[1];
+};
+
+const checkTimeMeeting = (startWork, endWork, startMeeting, durationMeeting) => {
+  startWork = convertsHoursToMinutes(startWork);
+  endWork = convertsHoursToMinutes(endWork);
+  startMeeting = convertsHoursToMinutes(startMeeting);
+  const endMeeting = startMeeting + durationMeeting;
+
+  return (startWork <= startMeeting && endWork >= endMeeting);
+};
+
+checkTimeMeeting('08:00', '17:30', '14:00', 90);
