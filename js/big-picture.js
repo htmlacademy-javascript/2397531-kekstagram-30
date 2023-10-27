c508806a9ee8cec321415c7ff80f54bf438db21a
Vysotskiy96 <tvgradskyj@gmail.com>
2023-10-27 19:40:27 +0300

    Добавляет загрузку комментариев в big-picture

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,40 +1,27 @@
 import {isEscapeKey} from './utils.js';
 
+const COMMENTS_STEP = 5;
+
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
 const likesCount = bigPicture.querySelector('.likes-count');
+const commentsShownCount = bigPicture.querySelector('.social__comment-shown-count');
 const commentsTotalCount = bigPicture.querySelector('.social__comment-total-count');
 const commentsList = bigPicture.querySelector('.social__comments');
 const captionPicture = bigPicture.querySelector('.social__caption');
 const cancelBigPicture = bigPicture.querySelector('.big-picture__cancel');
+const commentsLoader = bigPicture.querySelector('.social__comments-loader');
 
-const onCloseBigPictureClick = () => {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  cancelBigPicture.removeEventListener('click', onCloseBigPictureClick);
-  document.removeEventListener('keydown', onBigPictureEscapeKeydown);
-};
+let totalComments = [];
+let currentCountComments = 5;
 
-function onBigPictureEscapeKeydown (evt) {
-  if (isEscapeKey(evt)) {
-    onCloseBigPictureClick();
-    document.removeEventListener('keydown', onBigPictureEscapeKeydown);
-  }
-}
+const renderComments = (comments) => {
 
-
-const showBigPicture = (picture) => {
-  bigPicture.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-
-  bigPictureImg.src = picture.url;
-  likesCount.textContent = picture.likes;
-  captionPicture.textContent = picture.description;
-  commentsTotalCount.textContent = picture.comments.length;
+  const showComments = comments.slice(0, (currentCountComments >= comments.length) ? comments.length : currentCountComments);
 
   commentsList.innerHTML = '';
 
-  picture.comments.forEach((comment) => {
+  showComments.forEach((comment) => {
     commentsList.innerHTML += `
     <li class="social__comment">
     <img
@@ -47,9 +34,53 @@ const showBigPicture = (picture) => {
     `;
   });
 
-  cancelBigPicture.addEventListener('click', onCloseBigPictureClick);
-  document.addEventListener('keydown', onBigPictureEscapeKeydown);
+  if (currentCountComments >= totalComments.length) {
+    commentsLoader.classList.add('hidden');
+  }
+
+  commentsShownCount.textContent = showComments.length;
 };
 
+const onCommentsLoaderClick = () => {
+  currentCountComments += COMMENTS_STEP;
+  renderComments(totalComments);
+};
+
+const onCloseBigPictureClick = () => {
+  currentCountComments = 5;
+
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  commentsLoader.classList.remove('hidden');
+
+  cancelBigPicture.removeEventListener('click', onCloseBigPictureClick);
+  document.removeEventListener('keydown', onBigPictureEscapeKeydown);
+  commentsLoader.removeEventListener('click', onCommentsLoaderClick);
+};
+
+function onBigPictureEscapeKeydown (evt) {
+  if (isEscapeKey(evt)) {
+    onCloseBigPictureClick();
+    document.removeEventListener('keydown', onBigPictureEscapeKeydown);
+  }
+}
+
+const showBigPicture = (picture) => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  bigPictureImg.src = picture.url;
+  likesCount.textContent = picture.likes;
+  captionPicture.textContent = picture.description;
+  commentsTotalCount.textContent = picture.comments.length;
+
+  totalComments = picture.comments.slice();
+
+  renderComments(totalComments);
+
+  cancelBigPicture.addEventListener('click', onCloseBigPictureClick);
+  document.addEventListener('keydown', onBigPictureEscapeKeydown);
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
+};
 
 export {showBigPicture};
