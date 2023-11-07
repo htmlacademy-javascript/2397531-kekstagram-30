e1b94ec70312ccd286831f1de9d95226b08fb8b7
Vysotskiy96 <tvgradskyj@gmail.com>
2023-11-07 18:54:27 +0300

    Добавляет слайдер для эффектов

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {photos, createPhotos} from './generate-photos.js';
 import {renderPhotos} from './render-photos.js';
 import './form.js';
 import './check-hashtags.js';
+import './effects.js';
 
 createPhotos();
 renderPhotos(photos);
